1109b62769ad860238da8f477cd81f4517e8dfc1
Luis Carranza <carranlu@gmail.com>
2021-11-04 19:43:25 -0600

    Initial Commit

---
+++ SimpleList/SimpleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleListApp.swift
+//  SimpleList
+//
+//  Created by Luis Javier Carranza on 04/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
